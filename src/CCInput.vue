b2419bc94dcec0fb07316aa133b49ec5f97f0454
Cecilia Torres <cecilia.torres@diginteligencia.com>
2019-07-29 14:57:37 -0500

    luhn algorithm checked implemented

--- src/CCInput.vue
+++ src/CCInput.vue
@@ -225,7 +225,29 @@ export default {
         return false;
       }
 
-      return testPassed.some(test => test === true);
+      // The Luhn Algorithm.
+      const luhnCheck = num => {
+        if (/[^0-9-\s]+/.test(num)) return false;
+
+        var nCheck = 0, nDigit = 0, bEven = false;
+        num = num.replace(/\D/g, "");
+
+        for (var n = num.length - 1; n >= 0; n--) {
+          var cDigit = num.charAt(n);
+            nDigit = parseInt(cDigit, 10);
+
+          if (bEven) {
+            if ((nDigit *= 2) > 9) nDigit -= 9;
+          }
+
+          nCheck += nDigit;
+          bEven = !bEven;
+        }
+
+        return (nCheck % 10) == 0;
+      };
+
+      return testPassed.some(test => test === true) && luhnCheck(number);
     },
     validExp() {
       return this.expYear > CURRENT_YEAR
