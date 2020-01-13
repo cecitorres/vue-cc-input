7d51d21a8eb4a5308545254ab8a69bf49c7654df
Cecilia Torres <cecilia.torres@diginteligencia.com>
2020-01-13 15:49:04 -0600

    remove 2019 as option

--- src/CCInput.vue
+++ src/CCInput.vue
@@ -56,7 +56,6 @@
             @change="handlerBluredCardExp"
           >
             <option value="">Año</option>
-            <option value="2019">2019</option>
             <option value="2020">2020</option>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
@@ -67,6 +66,7 @@
             <option value="2027">2027</option>
             <option value="2028">2028</option>
             <option value="2029">2029</option>
+            <option value="2030">2030</option>
           </select>
         </div>
         <!-- <p v-if="cardExpBlured" class="error">El campo fecha es requerido</p> -->  
